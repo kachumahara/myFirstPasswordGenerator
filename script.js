4b91a0036ece9279a9752b5ca8922c73caff8bce
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-02-05 14:48:32 -0700

    added some functions

--- script.js
+++ script.js
@@ -3,39 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var params = {
+    length: prompt("How long would you like your password to be?"),
+    lowercase: confirm("Would you like to use lowercase characters?"),
+    uppercase: confirm("Would you like to use uppercase characters?"),
+    specialChars: confirm("Would you like to use special characters?"),
+    numericChars: confirm("Would you like to use numeric characters?")
+  };
+  var password = generatePassword(params);
   var passwordText = document.querySelector("#password");
-  var passwordLenght = prompt("How many characters would you like?")
-  var uppercase = confirm("would you like to use uppercase characters?")
-  var lowercase = confirm("would you like to use lowercase characters?")
-  var numericNumbers = confirm("would you like to ise numeric characters?")
-  var specialcharacters = confirm("would you like to use special?" )
-
   passwordText.value = password;
 }
-function generatePassword(){
-getNumbers();
-getLowerCases();
-getUpperCases();
-getSpecialCharacters();   
-
-   
+function generatePassword(options) {
+  if(options.uppercase){
+  }
+  return "SuperSecretPassword1098";
 }
 
-function getNumbers() {
-  return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-}
-
-function getLowerCases() {
-  return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-}
-
-function getUpperCases() {
-  return +String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-}
-
-function getSpecialCharacters() {
-  return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
-}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
