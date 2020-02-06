d7be37be9a70ed8ce045d5683cbe9e36250b5786
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-02-05 19:19:01 -0700

    correct some functions

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var params = {
-    length: prompt("How long would you like your password to be?"),
-    lowercase: confirm("Would you like to use lowercase characters?"),
-    uppercase: confirm("Would you like to use uppercase characters?"),
-    specialChars: confirm("Would you like to use special characters?"),
-    numericChars: confirm("Would you like to use numeric characters?")
-  };
-  var password = generatePassword(params);
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+	var params = {
+		length: prompt('How long would you like your password to be?'),
+		lowercase: confirm('Would you like to use lowercase characters?'),
+		uppercase: confirm('Would you like to use uppercase characters?'),
+		specialChars: confirm('Would you like to use special characters?'),
+		numericChars: confirm('Would you like to use numeric characters?'),
+	};
+	var password = generatePassword(params);
+	var passwordText = document.querySelector('#password');
+	passwordText.value = password;
 }
 function generatePassword(options) {
-  if(options.uppercase){
-  }
-  return "SuperSecretPassword1098";
+	getNumbers();
+	getLowerCases();
+	getUpperCases();
+	getSpecialCharacters();
+	function GetNumbers() {
+		return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+	}
+
+	function getLowerCases() {
+		return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+	}
+
+	function getUpperCases() {
+		return +String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+	}
+
+	function getSpecialCharacters() {
+		return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
+	}
+
+	if (options.numbers) {
+		numbers === true;
+		lowercase === true;
+		uppercase === true;
+		specialcharacters === true;
+		return;
+	}
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
