3fbc598d589eb24f3e6ae2e30409ffb51b93872c
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-02-06 21:43:05 -0700

    reasigning function and updating my files

--- script.js
+++ script.js
@@ -4,45 +4,20 @@ var generateBtn = document.querySelector('#generate');
 // Write password to the #password input
 function writePassword() {
 	var params = {
-		length: prompt('How long would you like your password to be?'),
 		lowercase: confirm('Would you like to use lowercase characters?'),
 		uppercase: confirm('Would you like to use uppercase characters?'),
 		specialChars: confirm('Would you like to use special characters?'),
 		numericChars: confirm('Would you like to use numeric characters?'),
 	};
+	while (params.length < 8 || params.length > 128 || params.length === undefined) {
+		options.length = prompt('How long would you like your password to be?');
+	}
 	var password = generatePassword(params);
 	var passwordText = document.querySelector('#password');
+
 	passwordText.value = password;
 }
-function generatePassword(options) {
-	getNumbers();
-	getLowerCases();
-	getUpperCases();
-	getSpecialCharacters();
-	function GetNumbers() {
-		return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-	}
-
-	function getLowerCases() {
-		return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-	}
-
-	function getUpperCases() {
-		return +String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-	}
-
-	function getSpecialCharacters() {
-		return specialCharacters(Math.floor(Math.random() * specialCharacters.length));
-	}
-
-	if (options.numbers) {
-		numbers === true;
-		lowercase === true;
-		uppercase === true;
-		specialcharacters === true;
-		return;
-	}
-}
+function generatePassword(options) {}
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
